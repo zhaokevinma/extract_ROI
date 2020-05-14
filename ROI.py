96f0991d661360e36eb69b88f31cc872bfd0a115
Kevin Malone <zhaokevinma@gmail.com>
2020-05-14 19:42:15 -0400

    flipped h w

--- ROI.py
+++ ROI.py
@@ -19,7 +19,7 @@ def image_to_numpyarr(image_path):
     else:
         print("Unknown mode: %s" % image.mode)  # Raise exception if 'image.mode' is neither 'RGB' nor 'L'
         return None
-    pixel_values = np.array(pixel_values).reshape((width, height, channels))  # Reshape List[tuples] to numpy.ndarray
+    pixel_values = np.array(pixel_values).reshape((height, width, channels))  # Reshape List[tuples] to numpy.ndarray
     return pixel_values
 
 def isRed(pixel):
@@ -31,10 +31,36 @@ def isRed(pixel):
     red = pixel[0]
     green = pixel[1]
     blue = pixel[2]
-    if red - green > 50 and red - blue > 50:  # There is more red in a red pixel 
+    if red - green > 40 and red - blue > 40:  # There is more red in a red pixel 
         return True
     else:  # A normal CT pixel has similar/equal R G B values
         return False
+
+def isBlue(pixel):
+    """
+    Tell whether a pixel is a blue pixel
+    type pixel: numpy.ndarray 
+    rtype: bool
+    """ 
+    red = pixel[0]
+    green = pixel[1]
+    blue = pixel[2]
+    if red == 0 and green == 0 and blue == 255:  # [0, 0, 255]
+        return True
+    else:  # A normal CT pixel has similar/equal R G B values
+        return False
+
+def repaint_contour(pixels):
+    """
+    Repaint the contour to blue
+    type pixel: numpy.ndarray 
+    rtype: pixel numpy.ndarray 
+    """ 
+    for i in range(len(pixels)):
+        for j in range(len(pixels[0])):
+            if isRed(pixels[i][j]):
+                pixels[i][j] = [0, 0, 255]
+    return pixels
 
 def mp(pixels):
     """
@@ -44,15 +70,16 @@ def mp(pixels):
     """ 
     mp = []
     for i in range(len(pixels)):
-        pos = [0]
-        new = []
+        # pos = [0]
+        pos = []
+        # new = []
         for j in range(len(pixels[0])):  # First loop we add every red pixel to pos
-            if isRed(pixels[i][j]):
+            if isBlue(pixels[i][j]):
                 pos.append(j)
-        for p in range(1, len(pos)):  # Second loop we filter the pos such that continuous red pixels only have 1 mark
-            if pos[p] - pos[p-1] > 10:
-                new.append(pos[p])
-        mp.append(new)
+        # for p in range(1, len(pos)):  # Second loop we filter the pos such that continuous red pixels only have 1 mark
+        #     if pos[p] - pos[p-1] > 10:
+        #         new.append(pos[p])
+        mp.append(pos)
     return mp
 
 def mask(pixels, mp):
@@ -63,10 +90,11 @@ def mask(pixels, mp):
     """ 
     for i in range(len(pixels)):
         if len(mp[i]) > 1:
-            for j in range(mp[i][0], mp[i][1]):
-                if isRed(pixels[i][j]) == False:
-                    if (len(mp[i]) - bisect.bisect_right(mp[i], j)) % 2 == 1: # If we find out that the intersection happens odd times we know it's inside
-                        pixels[i][j] = [0, 255, 0]  # If it's inside the polygon we paint it green
+            for j in range(mp[i][0]+1, mp[i][-1]):
+                # if isRed(pixels[i][j]) == False:
+                #     if (len(mp[i]) - bisect.bisect_right(mp[i], j)) % 2 == 1: # If we find out that the intersection happens odd times we know it's inside
+                pixels[i][j] = [0, 255, 0]  # If it's inside the polygon we paint it green
+
     return pixels
 
 def flatten_numpyarr(pixels):
@@ -90,7 +118,10 @@ if __name__ == "__main__":
     im = Image.new('RGB', (image.size[0], image.size[1]))
 
     pixels = image_to_numpyarr(image_path)
-    mapped = mp(pixels)
+    blued = repaint_contour(pixels)
+    mapped = mp(blued)
+
+    print(mapped)
     masked = mask(pixels, mapped)
     flattened = flatten_numpyarr(masked)
 
