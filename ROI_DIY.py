214e6d9deec11560f06b7ad6fad55dc938ee9568
Kevin Malone <zhaokevinma@gmail.com>
2020-05-15 15:43:22 -0400

    Code complete

--- ROI_DIY.py
+++ ROI_DIY.py
@@ -1,8 +1,9 @@
 from PIL import Image
 import numpy as np
-from scipy import ndimage
 from matplotlib import pyplot as plt
 from numba import jit
+import copy
+import bisect
 from warnings import filterwarnings
 filterwarnings('ignore')
 
@@ -17,17 +18,14 @@ def main():
     # # Display the original data for testing purposes
     # displayOriginal(pixels)
 
-    # Get ROI
-    roi = getROI(pixels)
+    # Map the boundaries and thinning them on each row
+    mapped = mp(pixels)
 
-    # ROI returns a polyline. Fill this in using the scipy ndimage binary fill holes function
-    mask = ndimage.binary_fill_holes(roi).astype(int)
-
-    # Apply mask to green channel
-    masked = maskOriginal(pixels, mask)
+    # Green the pixels enclosed by the red marks
+    masked = mask(pixels, mapped)
 
     # Display the masked results
-    displayResults(roi, mask, masked)
+    displayResults(pixels, masked)
 
     # Flatten the numpy arr
     flattened = flatten_numpyarr(masked)
@@ -35,7 +33,7 @@ def main():
     # Save final masked image
     im = Image.new('RGB', (image.size[0], image.size[1]))
     im.putdata(flattened)
-    im.save('ROI_SciPy.jpeg')
+    im.save("ROI_DIY.jpeg")
 
 
 @jit
@@ -75,40 +73,56 @@ def isRed(pixel, sensitivity=50):
     else:
         return False
 
+@jit
+def mp(pixels):
+    """
+    Create a map where red pixels's position in a row is recorded
+    """ 
+    # Initialize the map
+    mp = []
+
+    # For each row in the given numpy array
+    for i in range(len(pixels)):
+        # Initialize a list with 0
+        pos = [0]
+        # First loop we add every red pixel to pos
+        for j in range(len(pixels[0])):  
+            if isRed(pixels[i][j]):
+                pos.append(j)
+        # Initialize an empty list
+        new = []
+        # Second loop we filter the pos such that continuous red pixels only have 1 mark
+        for p in range(1, len(pos)): 
+            # If a red pixel is at least 10 pixel apart from the previous cluster we mark it
+            if pos[p] - pos[p-1] > 10:
+                new.append(pos[p])
+        # Map[i] is now a list of thinned markers
+        mp.append(new)
+    return mp
+
 
 @jit
-def getROI(pixels, sensitivity=50):
+def mask(pixels, mp):
     """
-    Get ROI from original RGB image assuming it is red on a grayscale image
-    """
-    # Get size of image
-    y, x = pixels.shape[:2]
+    Paint a pixel as green if it is inside 
+    """ 
+    # We don't have to deep copy but we are displaying 2 graphs side by side, manipulating the original pixel would make them the same
+    pixels = copy.deepcopy(pixels)
 
-    # Initialize output array
-    roi = np.zeros((y, x), dtype=int)
-
-    # Double for loop to go through each pixel in image
-    for i in range(x):
-        for j in range(y):
-            # If red value is greater than green and blue values within a sensitivity, will mark as ROI
-            if isRed(pixels[j, i]):
-                roi[j, i] = 1
-    return roi
-
-
-@jit
-def maskOriginal(pixels, mask):
-    # Get size of image
-    y, x = pixels.shape[:2]
-
-    # Double for loop to go through each pixel in image
-    for i in range(x):
-        for j in range(y):
-            # If the mask is present at a value, make green channel 255 and red and blue 0
-            if mask[j, i] == 1:
-                pixels[j, i, 0] = 0
-                pixels[j, i, 1] = 255
-                pixels[j, i, 2] = 0
+    # For each row of the image pixels
+    for i in range(len(pixels)):
+        # If our map indicates that there is at least 2 red pixel clusters
+        if len(mp[i]) > 1:
+            # From the leftmost red pixel to the rightmost red pixel 
+            for j in range(mp[i][0]+1, mp[i][-1]):
+                # If it is not a red pixel
+                if isRed(pixels[i][j]) == False:
+                    # 'insrt' gives us the left index in a sorted list where we would insert the value
+                    insrt = bisect.bisect_left(mp[i], j)
+                    # Using 'insrt' we can tell how many points to the left and right of the pixel are red -> odd for either means inside
+                    if insrt % 2 == 1 or (len(mp[i]) - insrt) % 2 == 1:
+                        # Paint it green for visual purposes
+                        pixels[i][j] = [0, 255, 0]
     return pixels
 
 
@@ -151,20 +165,17 @@ def displayOriginal(pixels):
     plt.show()
 
 
-def displayResults(roi, mask, pixels):
+def displayResults(pixels, masked):
     """
-    Display ROI, mask, and masked original image
+    Display original and masked original image
     """
     fig = plt.figure()
     fig.suptitle('Results')
-    fig.add_subplot(1, 3, 1)
-    plt.imshow(roi, cmap='gray')
-    plt.title('ROI')
-    fig.add_subplot(1, 3, 2)
-    plt.imshow(mask, cmap='gray')
-    plt.title('Mask')
-    fig.add_subplot(1, 3, 3)
+    fig.add_subplot(1, 2, 1)
     plt.imshow(pixels)
+    plt.title('Original')
+    fig.add_subplot(1, 2, 2)
+    plt.imshow(masked)
     plt.title('Masked Original Image')
     plt.show()
 
--- nibabel_loadHeader.py
+++
@@ -1,8 +0,0 @@
-import nibabel as nib
-
-# Read file 
-file = 'example.nii'
-img = nib.load(file)    
-
-# Read and print headers
-print(img.header)
